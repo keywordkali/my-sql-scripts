c7ef76a6b5fc7d0dfbe5e562db94ae83750a82bc
Kali Correll <kalicorrell18@gmail.com>
2020-09-18 16:08:13 -0400

    CreateTableExample

---
+++ CreateTableExample.sql
@@ -0,0 +1,5 @@
+insert students1
+(Id, Name, Email, Phone, Course)
+Values
+('0', 'Karen', 'Karen@gmail.com', '5132952615', 'Java'),
+('0', 'Kyle', 'Kyle@gmail.com', '5132785678', 'Csharp');
---
+++ UpdateExample3.sql
@@ -0,0 +1,5 @@
+select * from student where sat between 1000 and 1300
+
+update student set
+gpa = gpa + 0.1
+where sat between 1000 and 1300
